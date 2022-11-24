19b6528546187c48b1037ef500bf66f5659e65a2
Jonathan Reisdorf <jonathan.reisdorf@data4life.care>
2022-11-24 17:19:24 +0100

    🚀 fix base url for github page

--- sample-app/vite.config.js
+++ sample-app/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+const BASE_URL = process.env.BASE_URL || "";
+
 export default defineConfig({
   plugins: [vue()],
+  base: `${BASE_URL}/`,
 });
